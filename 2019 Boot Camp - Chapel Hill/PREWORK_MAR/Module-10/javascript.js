f71d6b8bb26d9ecb656e0dddbccd30a97a9ac63b
Alex Rhinehart <rhinehart.alex05@gmail.com>
2019-02-04 18:15:53 -0500

    added my homework folder

---
+++ 2019 Boot Camp - Chapel Hill/PREWORK_MAR/Module-10/javascript.js
@@ -0,0 +1,28 @@
+<script type="text/javascript" ></script>
+
+document.getElementById("grow").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "200px";
+
+});
+
+        document.getElementById("blue").addEventListener("click", function(){
+
+            document.getElementById("box").style.color = "blue"; 
+
+});
+
+        document.getElementById("fade").addEventListener("click", function(){
+
+            document.getElementById("box").fadeIn = "1";
+
+});
+
+        document.getElementById("reset").addEventListener("click", function(){
+
+            document.getElementById("box").reset;
+
+});
+    
+
+    
